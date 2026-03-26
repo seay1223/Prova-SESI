a14255f8e061d0e37dd717d5fb8054080dd99557
samuel <reis68535@gmail.com>
2026-03-26 14:29:21 -0300

    UI login/cadastro completo

---
+++ public/js/redirect.js
@@ -0,0 +1,3 @@
+document.getElementById("btn-cadastro").addEventListener("click", function() {
+    window.location.href = "/cadastro";
+});
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.static('public'))
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
+app.use(express.static('public'));
+
+app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/public/html/login.html');
 });
 
+app.get('/cadastro', (req, res) => {
+  res.sendFile(__dirname + '/public/html/cadastro.html');
+});
+
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
-})
+    console.log(`http://localhost:${port}`);
+});
